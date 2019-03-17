0a18310b57b4fe64c81ef8394e39d8e3025d7ea0
Miles Robson <miles_robson@yahoo.com>
2019-03-16 20:52:22 -0400

    Finish Chapter 1 and flake8 lint changes

--- Chapter 1/Chapter1.py
+++ Chapter 1/Chapter1.py
@@ -1,15 +1,11 @@
-import sys
 import pandas as pd
-import matplotlib
 import numpy as np
-import scipy as sp
-import IPython
-import sklearn
 import mglearn
 import matplotlib.pyplot as plt
 from pandas.plotting import scatter_matrix
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 iris_dataset = load_iris()
 print("Keys of iris datset: \n {}".format(iris_dataset.keys()))
 print(iris_dataset['DESCR'][:193] + '\n...')
@@ -25,13 +21,37 @@ print("Target:\n", iris_dataset['target'])
 
 
 X_train, X_test, y_train, y_test = train_test_split(
-    iris_dataset['data'], iris_dataset['target'],random_state=0
+    iris_dataset['data'], iris_dataset['target'], random_state=0
 )
 
 print("X_train shape: {}".format(X_train.shape))
 print("y_train shape: {}".format(y_train.shape))
 
-iris_dataframe = pd.DataFrame(X_train,columns=iris_dataset.feature_names)
-df = pd.DataFrame(np.random.randn(1000, 4), columns=['A','B','C','D'])
-scatter_matrix(iris_dataframe,c=y_train,figsize=(15,15),marker='o',hist_kwds={'bins':20},s=60,alpha=.8,cmap=mglearn.cm3)
+
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+df = pd.DataFrame(np.random.randn(1000, 4), columns=['A', 'B', 'C', 'D'])
+scatter_matrix(
+    iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
+    hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
 plt.show()
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+X_new = np.array([[5, 2.9, 1, 0.2]])
+print("X_new shape: {}".format(X_new.shape))
+
+prediction = knn.predict(X_new)
+print("Prediction:", prediction)
+print("Predicted target name:", iris_dataset['target_names'][prediction])
+
+y_pred = knn.predict(X_test)
+print("Test set predictions:\n", y_pred)
+print("Test set score: {:.2f}".format(np.mean(y_pred == y_test)))
+print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
+
+X_train, X_test, y_train, y_test = train_test_split(
+    iris_dataset['data'], iris_dataset['target'], random_state=0)
+
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+
+print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
