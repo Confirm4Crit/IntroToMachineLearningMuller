b738f8e1c876f191fa9d569b19387a6a651a1fdd
Miles Robson <miles_robson@yahoo.com>
2019-04-07 20:57:55 -0400

    Linear Models

--- Chapter 2/Chapter2.py
+++ Chapter 2/Chapter2.py
@@ -8,6 +8,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.datasets import make_blobs
 
 # generate dataset
 X, y = mglearn.datasets.make_forge()
@@ -106,7 +109,6 @@ plt.show()
 mglearn.plots.plot_knn_regression(n_neighbors=3)
 plt.show()
 
-
 X, y = mglearn.datasets.make_wave(n_samples=40)
 
 # split the wave dataset into a training and a test set
@@ -119,7 +121,6 @@ reg.fit(X_train, y_train)
 
 print("Test set predictions:\n", reg.predict(X_test))
 print("Test set R^2: {:.2f}".format(reg.score(X_test, y_test)))
-
 
 # Analyzing KNeighborsRegressor
 fig, axes = plt.subplots(1, 3, figsize=(15, 4))
@@ -142,7 +143,6 @@ for n_neighbors, ax in zip([1, 3, 9], axes):
 axes[0].legend(["Model predictions", "Training data/target",
                 "Test data/target"], loc="best")
 plt.show()
-
 
 # Linear models for regression
 mglearn.plots.plot_linear_regression_wave()
@@ -200,6 +200,7 @@ mglearn.plots.plot_ridge_n_samples()
 plt.show()
 
 from sklearn.linear_model import Lasso
+
 lasso = Lasso().fit(X_train, y_train)
 print("Training set score: {:.2f}".format(lasso.score(X_train, y_train)))
 print("Test set score: {:.2f}".format(lasso.score(X_test, y_test)))
@@ -225,3 +226,120 @@ plt.ylim(-25, 25)
 plt.xlabel("Coefficient index")
 plt.ylabel("Coefficient magnitude")
 plt.show()
+
+X, y = mglearn.datasets.make_forge()
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 3))
+
+for model, ax in zip([LinearSVC(), LogisticRegression()], axes):
+    clf = model.fit(X, y)
+    mglearn.plots.plot_2d_separator(clf, X, fill=False, eps=0.5,
+                                    ax=ax, alpha=.7)
+    mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    ax.set_title(clf.__class__.__name__)
+    ax.set_xlabel("Feature 0")
+    ax.set_ylabel("Feature 1")
+axes[0].legend()
+
+plt.show()
+
+mglearn.plots.plot_linear_svc_regularization()
+
+plt.show()
+
+cancer = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(
+    cancer.data, cancer.target, stratify=cancer.target, random_state=42)
+logreg = LogisticRegression().fit(X_train, y_train)
+print("Training set score: {:.3f}".format(logreg.score(X_train, y_train)))
+print("Test set score: {:.3f}".format(logreg.score(X_test, y_test)))
+
+logreg100 = LogisticRegression(C=100).fit(X_train, y_train)
+print("Training set score: {:.3f}".format(logreg100.score(X_train, y_train)))
+print("Test set score: {:.3f}".format(logreg100.score(X_test, y_test)))
+
+logreg001 = LogisticRegression(C=0.01).fit(X_train, y_train)
+print("Training set score: {:.3f}".format(logreg001.score(X_train, y_train)))
+print("Test set score: {:.3f}".format(logreg001.score(X_test, y_test)))
+
+plt.plot(logreg.coef_.T, 'o', label="C=1")
+plt.plot(logreg100.coef_.T, '^', label="C=100")
+plt.plot(logreg001.coef_.T, 'v', label="C=0.001")
+plt.xticks(range(cancer.data.shape[1]), cancer.feature_names, rotation=90)
+xlims = plt.xlim()
+plt.hlines(0, xlims[0], xlims[1])
+plt.xlim(xlims)
+plt.ylim(-5, 5)
+plt.xlabel("Feature")
+plt.ylabel("Coefficient magnitude")
+plt.legend()
+
+plt.show()
+
+for C, marker in zip([0.001, 1, 100], ['o', '^', 'v']):
+    lr_l1 = LogisticRegression(C=C, penalty="l1").fit(X_train, y_train)
+    print("Training accuracy of l1 logreg with C={:.3f}: {:.2f}".format(
+        C, lr_l1.score(X_train, y_train)))
+    print("Test accuracy of l1 logreg with C={:.3f}: {:.2f}".format(
+        C, lr_l1.score(X_test, y_test)))
+    plt.plot(lr_l1.coef_.T, marker, label="C={:.3f}".format(C))
+
+plt.xticks(range(cancer.data.shape[1]), cancer.feature_names, rotation=90)
+xlims = plt.xlim()
+plt.hlines(0, xlims[0], xlims[1])
+plt.xlim(xlims)
+plt.xlabel("Feature")
+plt.ylabel("Coefficient magnitude")
+
+plt.ylim(-5, 5)
+plt.legend(loc=3)
+
+plt.show()
+
+X, y = make_blobs(random_state=42)
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+plt.xlabel("Feature 0")
+plt.ylabel("Feature 1")
+plt.legend(["Class 0", "Class 1", "Class 2"])
+
+plt.show()
+
+linear_svm = LinearSVC().fit(X, y)
+print("Coefficient shape: ", linear_svm.coef_.shape)
+print("Intercept shape: ", linear_svm.intercept_.shape)
+
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+line = np.linspace(-15, 15)
+for coef, intercept, color in zip(linear_svm.coef_, linear_svm.intercept_,
+                                  mglearn.cm3.colors):
+    plt.plot(line, -(line * coef[0] + intercept) / coef[1], c=color)
+plt.ylim(-10, 15)
+plt.xlim(-10, 8)
+plt.xlabel("Feature 0")
+plt.ylabel("Feature 1")
+plt.legend(['Class 0', 'Class 1', 'Class 2', 'Line class 0', 'Line class 1',
+            'Line class 2'], loc=(1.01, 0.3))
+
+plt.show()
+
+
+mglearn.plots.plot_2d_classification(linear_svm, X, fill=True, alpha=.7)
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+line = np.linspace(-15, 15)
+for coef, intercept, color in zip(linear_svm.coef_, linear_svm.intercept_,
+                                  mglearn.cm3.colors):
+    plt.plot(line, -(line * coef[0] + intercept) / coef[1], c=color)
+plt.legend(['Class 0', 'Class 1', 'Class 2', 'Line class 0', 'Line class 1',
+            'Line class 2'], loc=(1.01, 0.3))
+plt.xlabel("Feature 0")
+plt.ylabel("Feature 1")
+
+plt.show()
+
+# instantiate model and fit it in one line
+logreg = LogisticRegression().fit(X_train, y_train)
+
+logreg = LogisticRegression()
+y_pred = logreg.fit(X_train, y_train).predict(X_test)
+
+y_pred = LogisticRegression().fit(X_train, y_train).predict(X_test)
