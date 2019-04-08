bd8f945057c6130801ebd768ae7b732ac8be2d42
Miles Robson <miles_robson@yahoo.com>
2019-04-07 20:51:43 -0400

    Lasso

--- Chapter 2/Chapter2.py
+++ Chapter 2/Chapter2.py
@@ -198,3 +198,30 @@ plt.show()
 
 mglearn.plots.plot_ridge_n_samples()
 plt.show()
+
+from sklearn.linear_model import Lasso
+lasso = Lasso().fit(X_train, y_train)
+print("Training set score: {:.2f}".format(lasso.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lasso.score(X_test, y_test)))
+print("Number of features used: {}".format(np.sum(lasso.coef_ != 0)))
+
+lassoAlpha01 = Lasso(alpha=0.01, max_iter=100000).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(lassoAlpha01.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lassoAlpha01.score(X_test, y_test)))
+print("Number of features used: {}".format(np.sum(lassoAlpha01.coef_ != 0)))
+
+lassoAlpha00001 = Lasso(alpha=0.0001, max_iter=100000).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(lassoAlpha00001.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lassoAlpha00001.score(X_test, y_test)))
+print("Number of features used: {}".format(np.sum(lassoAlpha00001.coef_ != 0)))
+
+plt.plot(lasso.coef_, 's', label="Lasso alpha=1")
+plt.plot(lassoAlpha01.coef_, '^', label="Lasso alpha=0.01")
+plt.plot(lassoAlpha00001.coef_, 'v', label="Lasso alpha=0.0001")
+
+plt.plot(ridge01.coef_, 'o', label="Ridge alpha=0.1")
+plt.legend(ncol=2, loc=(0, 1.05))
+plt.ylim(-25, 25)
+plt.xlabel("Coefficient index")
+plt.ylabel("Coefficient magnitude")
+plt.show()
